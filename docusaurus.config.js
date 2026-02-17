32138bfdc43f2e41605a816c1d89622498945636
Novák Dániel <novakdaniel0805@gmail.com>
2026-02-17 14:51:31 +0100

    deploy docosaurus base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://identigo.hu',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/identigo-docosaurus/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
